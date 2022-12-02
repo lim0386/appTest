b474627ba8c5e7c7957fa9f83e9f6188cc7c9e3e
trumpetYK <lim0386@gmail.com>
2022-12-02 15:40:27 +0900

    changed

--- sketch.js
+++ sketch.js
@@ -1,28 +1,17 @@
-var wave;
-var playing =false;
-var button;
-
+let r, g, b;
 function setup() {
-  createCanvas(720, 256);
-  wave = new p5.Oscillator();
-  wave.setType('sine');
-  // wave.start();
-  wave.freq(440);
-  wave.amp(0);
-
- button = createButton('play/pause');
- button.mousePressed(toggle);
+  createCanvas(displayWidth, displayHeight);
+  r = random(50, 255);
+  g = random(0, 200);
+  b = random(50, 255);
 }
-function draw(){
-
+function draw() {
+  background(r, g, b);
+  console.log('draw’);
 }
-function toggle() {
-  if (!playing) {
-    wave.start();
-    wave.amp(0.5, 1);
-    playing = true;
-  } else {
-    wave.amp(0, 1);
-    playing = false;
-  }
+
+function deviceMoved() {
+  r = map(accelerationX, -90, 90, 100, 175);
+  g = map(accelerationY, -90, 90, 100, 200);
+  b = map(accelerationZ, -90, 90, 100, 200);
 }
