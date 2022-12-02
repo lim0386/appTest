fc3a5dbc466d7ba59b1f962a2d48ce07af115d3d
trumpetYK <lim0386@gmail.com>
2022-12-02 15:56:11 +0900

    text

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
 let r, g, b;
 function setup() {
   createCanvas(displayWidth, displayHeight);
-  r = random(50, 255);
-  g = random(0, 200);
-  b = random(50, 255);
+  // r = random(50, 255);
+  // g = random(0, 200);
+  // b = random(50, 255);
 }
 function draw() {
-  background(r, g, b);
+  // background(255);
   // console.log('draw’);
+  // text(accelerationX+", "+ accelerationY+", "+ accelerationZ);
 }
 
 function deviceMoved() {
-  r = map(accelerationX, -90, 90, 100, 175);
-  g = map(accelerationY, -90, 90, 100, 200);
-  b = map(accelerationZ, -90, 90, 100, 200);
+  background(255);
+  text(accelerationX+", "+ accelerationY+", "+ accelerationZ, 10, 10);
+  // r = map(accelerationX, -90, 90, 100, 175);
+  // g = map(accelerationY, -90, 90, 100, 200);
+  // b = map(accelerationZ, -90, 90, 100, 200);
 }
